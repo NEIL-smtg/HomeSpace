1c2c36dfa0b73f26a032398383562f27b528049c
NEIL-smtg <72685169+NEIL-smtg@users.noreply.github.com>
2021-12-13 23:10:06 +0800

    added functions for PropertyActionPage

--- app/src/main/java/com/example/homespace/PropertyActionPage.java
+++ app/src/main/java/com/example/homespace/PropertyActionPage.java
@@ -8,10 +8,12 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,9 +33,11 @@ public class PropertyActionPage extends AppCompatActivity {
     String id,pushID,itemType;
 
     //widget
-    CircleImageView back;
+    CircleImageView back, agentProPic;
     ViewPager viewPager;
-    TextView title,description;
+    TextView title,description,category,builtup, landArea, tenure, furnishing, roomNum, toiletNum, price;
+    Button addToCart, Chat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +53,27 @@ public class PropertyActionPage extends AppCompatActivity {
         itemType = agent.getItemType();
 
 
-        //widget
+        //property detail widget
         viewPager = (ViewPager) findViewById(R.id.slide_adapter_viewpager);
         title = (TextView) findViewById(R.id.property_title);
         description = (TextView) findViewById(R.id.property_description);
+        category = (TextView) findViewById(R.id.category);
+        builtup = (TextView) findViewById(R.id.builtup);
+        landArea = (TextView) findViewById(R.id.landArea);
+        tenure = (TextView) findViewById(R.id.tenure);
+        furnishing = (TextView) findViewById(R.id.furnishing);
+        roomNum = (TextView) findViewById(R.id.roomNum);
+        toiletNum = (TextView) findViewById(R.id.toiletNum);
+        price = (TextView) findViewById(R.id.price);
 
+        //bottom toolbar widget
+        addToCart = (Button) findViewById(R.id.addtocart);
+        Chat = (Button) findViewById(R.id.chat);
+
+        //agent
+        agentProPic = (CircleImageView) findViewById(R.id.agent);
+
+        //arrow back
         back = (CircleImageView) findViewById(R.id.action_page_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +84,7 @@ public class PropertyActionPage extends AppCompatActivity {
 
         DisplayImages();
         DisplayPropertyDetail();
+        SetupAgentProPic();
     }
 
 
@@ -100,15 +121,80 @@ public class PropertyActionPage extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot)
             {
+                //get detail and set
                 if (snapshot.child("description").exists())
                 {
                     description.setText(snapshot.child("description").getValue().toString());
                 }
+
+                if (snapshot.child("category").exists())
+                {
+                    category.setText(snapshot.child("category").getValue().toString());
+                }
+
+                if (snapshot.child("builtup").exists())
+                {
+                    builtup.setText(snapshot.child("builtup").getValue().toString() + " ft");
+                }
+
+                if (snapshot.child("landArea").exists())
+                {
+                    landArea.setText(snapshot.child("landArea").getValue().toString() + " ft");
+                }
+
+                if (snapshot.child("tenure").exists())
+                {
+                    tenure.setText(snapshot.child("tenure").getValue().toString());
+                }
+
+                if (snapshot.child("furnishing").exists())
+                {
+                    furnishing.setText(snapshot.child("furnishing").getValue().toString());
+                }
+
+                if (snapshot.child("rooms").exists())
+                {
+                    roomNum.setText(snapshot.child("rooms").getValue().toString());
+                }
+
+                if (snapshot.child("toilets").exists())
+                {
+                    toiletNum.setText(snapshot.child("toilets").getValue().toString());
+                }
+
+                if (snapshot.child("price").exists())
+                {
+                    price.setText("RM " + snapshot.child("price").getValue().toString());
+                }
+
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
         });
+    }
+
+    private void SetupAgentProPic()
+    {
+        String ProfilePicLink = agent.getProfilePic();
+        Glide
+                .with(PropertyActionPage.this)
+                .load(ProfilePicLink)
+                .into(agentProPic);
+
+        agentProPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start intent agentViewProfilePage
+                //pass agent....
+            }
+        });
+    }
+
+    private void addItemToCart()
+    {
+        //pass agent
+        //add animation
     }
 
 }
